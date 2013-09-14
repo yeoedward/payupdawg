128b1e666cd9c33017871a2ba3d6ec4b2aec1971
Edward Yeo <eddy@Edwards-MacBook-Air.local>
2013-09-14 19:48:58 -0400

    minimal changes

--- webapp/receipts/models.py
+++ webapp/receipts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Receipt(models.Model):
+    title = 
     date = models.DateField()
     category = models.CharField(max_length=30)
     #shared is a list of members
