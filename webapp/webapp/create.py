5630747000bc60688766e47d487f5d9246c42c24
Marisa Chang <mtchang@andrew.cmu.edu>
2013-09-15 04:55:03 +0800

    testing create.py

--- webapp/webapp/create.py
+++ webapp/webapp/create.py
@@ -1,6 +1,6 @@
 from django.contrib import auth
 from django.contrib.auth.models import User
 
-user = User.objects.create_user(username='Niki',email='nikitam8@gmail.com', '123456')
+user = User.objects.create_user(username='Niki',password='123456')
 
-print user.id
+print user.get_username()
