71683ad1e79e3ab23dbfce125e4c26e4a0a0cc4a
Marisa Chang <mtchang@andrew.cmu.edu>
2013-09-15 07:44:11 +0800

    created receipt database

--- webapp/receipts/models.py
+++ webapp/receipts/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Receipt(models.Model):
+    title = models.CharField(max_length=30)
     date = models.DateField()
+    price = models.FloatField(default=0.00)
     category = models.CharField(max_length=30)
+    
     #shared is a list of members
-    shared = models.DataList()
+    # NEED TO EDIT shared = models.ListField()
     owner = models.CharField(max_length=30)
 
