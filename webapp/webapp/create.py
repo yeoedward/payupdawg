ff9f55488bd18ec3c62867114e65298dddfba3eb
Marisa Chang <mtchang@andrew.cmu.edu>
2013-09-15 04:49:15 +0800

    this is really messed up

--- webapp/webapp/create.py
+++ webapp/webapp/create.py
@@ -1,2 +1,6 @@
-from django.db import models
+from django.contrib import auth
+from django.contrib.auth.models import User
 
+user = User.objects.create_user(username='Niki',email='nikitam8@gmail.com', '123456')
+
+print user.id
