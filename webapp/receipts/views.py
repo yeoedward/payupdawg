4fd9f986cae5a7c20a91341af72ef688b1741fe5
Eddy Yeo <edwardyeo@cmu.edu>
2013-09-15 11:49:52 -0400

    Done

--- webapp/receipts/views.py
+++ webapp/receipts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
+from django.core.exceptions import ValidationError
 from receipts.models import *
 
 def goHome(request):
@@ -50,7 +51,6 @@ def invalid(request):
 
 # populate table in request page
 
-#TODO: doesn't work, filter receipts
 def receipts(request):
 #  def username(receipt):
 #    receipt.owner = receipt.owner.all()[0].username
@@ -68,18 +68,72 @@ def newreceipt(request):
   totalPrice = request.POST.get('totalPrice')
   category = request.POST.get('category')
   owner = Dawg.objects.get(username=request.user.username)
-  groups = Homies.objects.get(id=request.POST.get('groups'))
-  r = Receipt(title=title, date=date, totalPrice=totalPrice, category=category)
-  r.save()
-  r.owner.add(owner)
-  r.groups.add(groups)
-  r.save()
+  try:
+    groups = Homies.objects.get(id=request.POST.get('groups'))
+    r = Receipt(title=title, date=date, totalPrice=totalPrice, category=category)
+    r.save()
+    r.owner.add(owner)
+    r.groups.add(groups)
+    r.save()
+  except ValidationError:
+    pass
   return HttpResponseRedirect("receipts")
 
 def groups(request):
   group_list = list(Homies.objects.filter(dawgs__username__exact=
                                             request.user.username))
   return render(request, "groups.html", {'group_list' : group_list})
+
+def insert((x,m),L):
+  if L == []:
+    return [(x,m)]
+  (a,m2) = L.pop(0)
+  if x <= a:
+    return [(x,m),(a,m2)]+L
+  return [(a,m2)] + insert((x,m),L)
+
+def del_eql(L):
+  trans = []
+  to_del = set()
+  S = {}
+  for (i,(n,m)) in enumerate(L):
+    if n == 0:
+      to_del.add(i)
+    if -n in S:
+      to_del.add(S[-n][0])
+      to_del.add(i)
+      payer = m if i > S[-n][0] else S[-n][1]
+      payee = S[-n][1] if i > S[-n][0] else m
+      trans += [(m,S[-n][1],abs(n))]
+      del S[-n]
+    else:
+      S[n] = (i,m)
+  result = []
+  for (i,p) in enumerate(L):
+    if i in to_del:
+      continue
+    result += [p]
+  return (result,trans)
+
+def matchmake(L):
+  if L is None: return None
+  (result,trans) = del_eql(L)
+  if len(result) == 0: return result + trans
+  assert(len(result) > 1)
+  (n1,m1) = result.pop()
+  (n2,m2) = result.pop(0)
+  new_n = n1 + n2
+  new_m = m1 if abs(n1) >= abs(n2) else m2
+  changed_hands = min(abs(n1),abs(n2))
+  payer = m1 if n1 > 0 else m2
+  payee = m2 if n1 > 0 else m1
+  return trans + [(payer,payee,changed_hands)] + matchmake(insert((new_n,new_m),result))
+
+
+def stringify(L):
+  if L is None: return None
+  return map(lambda (a,b,n): a+" pays $"+str(n)+" to "+b+".", L)
+
 
 def group(request,group_id):
   g = Homies.objects.get(id = group_id)
@@ -111,9 +165,15 @@ def group(request,group_id):
   peopleL.sort(key=lambda (_,x): x)
 
   owners = map(lambda r: r.owner.all()[0].username, receipt_list)
+  zipped = zip(receipt_list, owners)
 
-  return render(request, "group.html", {'avg' : avg, 'people' : peopleL, 'zipped' : zip(receipt_list,owners),
-                                        'gid' : group_id} )
+  net_spend = sorted(map(lambda (o,r): (avg-r, o), peopleL), key = lambda (n,_): n)
+  try:
+    transactions = stringify(matchmake(net_spend))
+  except Exception:
+    transactions = None
+  return render(request, "group.html", {'avg' : avg, 'people' : peopleL, 'zipped' : zipped,
+                                        'gid' : group_id, 'trans' : transactions} )
 
 def creategroup(request):
   name = request.POST.get('name')
